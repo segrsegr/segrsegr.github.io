90c06df1cdd7329491523d6226e151a690fd997e
segrsegr <42430545+segrsegr@users.noreply.github.com>
2020-09-01 13:59:42 +0400

    Add files via upload

---
+++ precache-manifest.1c853244028c2ab41551a7575ca1ce43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecb6479fd1d77d2780121f0e4aa5dac0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be61af812c0be593459e",
+    "url": "/static/css/main.f1e82829.chunk.css"
+  },
+  {
+    "revision": "a832824b9c2697a06c97",
+    "url": "/static/js/2.877b28ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.877b28ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be61af812c0be593459e",
+    "url": "/static/js/main.6cdab7c3.chunk.js"
+  },
+  {
+    "revision": "d7b39ccc50ed8f38c68e",
+    "url": "/static/js/runtime-main.1ef7d278.js"
+  }
+]);
